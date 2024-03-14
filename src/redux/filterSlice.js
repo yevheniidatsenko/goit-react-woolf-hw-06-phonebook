ea1d9723090543e5e673571495b0141cebd6754a
Yevhenii Datsenko <yevheniydatsenko@gmail.com>
2024-03-14 22:30:28 +0200

    ❇️ Updated filterSlice

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -7,7 +7,7 @@ const filterSlice = createSlice({
   initialState: initialFilterState,
   reducers: {
     changeFilter(state, action) {
-      state = action.payload;
+      return action.payload;
     },
   },
 });
